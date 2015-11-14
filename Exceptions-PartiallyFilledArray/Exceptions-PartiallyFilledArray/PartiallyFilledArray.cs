b67498520550ea56d85cc170dc0987bed8f9590b
Alex Carstensen <matthacker.alex@gmail.com>
2015-11-14 17:29:54 +0100

    init

---
+++ Exceptions-PartiallyFilledArray/Exceptions-PartiallyFilledArray/PartiallyFilledArray.cs
@@ -0,0 +1,32 @@
+﻿namespace PartiallyFilledArray
+{
+    class PartiallyFilledArray : IPartiallyFilledArray
+    {
+        public uint Size { get; }
+        public uint Used { get; }
+        public void Set(uint pos, int data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Get(uint pos)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public uint Put(int data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public uint Find(int data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveAt(uint pos)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
